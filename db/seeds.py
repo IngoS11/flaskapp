756698d3f4b5b72af2c41f72b10327ffa1f08d95
Ingo Sauerzapf <ingo.sauerzapf@gmail.com>
2022-05-29 03:53:01 +0000

    added sees database data

--- db/seeds.py
+++ db/seeds.py
@@ -1,27 +1,31 @@
 from app.database import db, User, Bookmark
 
 # add some users to the database
-users = []
-users.append(User('peterpan', 'peter@neverland.org', 'neverland'))
-users.append(User('tinker', 'tinker@neverland.org', 'neverland'))
-users.append(User('wendy', 'wendy@neverland.org', 'neverland'))
+users = [
+    { "username": "peter", "email": "peter@neverland.org", "password": "abcd1234"},
+    { "username": "wendy", "email": "wendy@neverland.org", "password": "abcd1234"},
+    { "username": "tinker", "email": "tinker@neverland.org", "password": "abcd1234"},
+]
+
+bookmarks = [
+    { "url" : "http://google.com", "body": "Google"},
+    { "url" : "http://yahoo.com", "body": "Yahoo"},
+    { "url" : "http://bing.com", "body": "Bing"},
+    { "url" : "http://duckduckgo.com", "body": "Duck Duck Go"},
+    { "url" : "http://someother.com", "body": "SomeOther"},
+    { "url" : "http://linked.in", "body": "LinkedIn"},
+]
 
 for user in users:
-    if User.query.filter_by(username=user.username).first() is None:
-        u=User(user.username, user.email, user.password)
-        db.session.add(u)
-        db.session.commit()
 
-        # add the same bookmarks to all users in the database
-        bookmarks = []
-        bookmarks.append(Bookmark('http://google.com', 'Google', u.id))
-        bookmarks.append(Bookmark('http://yahoo.com', 'Yahoo', u.id))
-        bookmarks.append(Bookmark('http://bing.com', 'Bing', u.id))
-        bookmarks.append(Bookmark('http://duckduckgo.com', 'DuckDuckGo', u.id))
-        bookmarks.append(Bookmark('http://doesnotexist.com', 'Does Not Exist', u.id))
+    if User.query.filter_by(username=user["username"]).first() is None:
+        usr = User(user["username"], user["email"], user["password"])
+        db.session.add(usr)
+        #db.session.commit()
 
         for bookmark in bookmarks:
-            if Bookmark.query.filter_by(url=bookmark.url, user_id=u.id).first() is None:
-                db.session.add(bookmark)
-
-db.session.commit()
+            if Bookmark.query.filter_by(url=bookmark["url"], user_id=usr.id).first() is None:
+                book = Bookmark(bookmark["url"], bookmark["body"], usr.id)
+                db.session.add(book)
+            
+        db.session.commit()
