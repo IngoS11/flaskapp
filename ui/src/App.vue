a0896d0400321fe05c98534baf48cde557bac1f4
Ingo Sauerzapf <ingo.sauerzapf@gmail.com>
2022-06-14 15:57:35 +0000

    Commited Vue Learning Session

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">ß
+  <h1>Ninja Reaction Timer</h1>
+  <p>See how good your reaction is</p>
+  <button @click="startGame" :disabled="isPlaying">Start Game</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    startGame(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+    endGame(reactionTime){
+      
+    }
+ } 
 }
 </script>
 
@@ -17,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
